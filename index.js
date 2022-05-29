2fe366c971ad824143504822d1ff560efcd134f1
Pouya <pouya.eghbali@yandex.com>
2022-05-29 12:20:24 +0200

    Add apikey to graph query

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import axios from "axios";
  * This is the Kenshi Deep Index endpoint for GraphQL
  */
 const endpoint = "https://api.kenshi.io/index/graphql";
+const apikey = "API_KEY";
+const owner = "API_KEY_OWNER";
 
 /**
  * Define your GraphQL query here
  */
 const query = `{
-    getEntries(blockchain: "binance-testnet") {
+    getEntries(blockchain: "binance-testnet", apikey: "${apikey}", owner: "${owner}") {
       event {
         name
       }
@@ -20,5 +22,5 @@ const query = `{
  * Post request sending the GraphQL query to the
  * Kenshi Deep Index endpoint, receive the data and print
  */
-const { data } = await axios.post(endpoint, { query });
+const data = await axios.post(endpoint, { query });
 console.log(data);
